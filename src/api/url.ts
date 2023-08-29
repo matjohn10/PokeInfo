244dfb0801154c2149f3504e0ca0511e2fe36bab
Mathieu Johnson <117942546+matjohn10@users.noreply.github.com>
2023-08-28 23:41:42 -0400

    Update url.ts

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,4 +1,4 @@
-const BASEURL = "http://127.0.0.1:5000";
+const BASEURL = "https://pokeinfo-api.onrender.com";
 const BACKEND = "https://pokeinfo-server.onrender.com/"; // Change before deploying frontend "http://localhost:3000/"
 const isDeployed = true;
 
